e354a1fa06dc5729f02fcf0b166ddb7a5104db4c
yanignacio <yandasilva3@gmail.com>
2026-03-28 09:00:42 -0300

    O carro anda agora como esperado

--- go-to-town-game/scripts/car.gd
+++ go-to-town-game/scripts/car.gd
@@ -1,20 +1,17 @@
 extends RigidBody2D
 
-const SPEED = 3000.0
+const TORQUE = 10000.0
 const FRICTION = 10
 const JUMP_VELOCITY = 0
 const MAX_SPEED = 900
 
-@onready var frontWheel = $frontWheel
-@onready var backWheel = $backWheel
-
-func _ready():
-	frontWheel.apply_torque(10000)
+@onready var frontWheel = $frontWheelPin/frontWheel
+@onready var backWheel = $backWheelPin/backWheel
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		frontWheel.apply_torque(direction * SPEED)
-		backWheel.apply_torque(direction * SPEED)
+		frontWheel.apply_torque(direction * TORQUE)
+		backWheel.apply_torque(direction * TORQUE)
