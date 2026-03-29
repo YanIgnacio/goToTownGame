015425e9b4d940753777291a1499f47da834ac0a
Andrew <andrewcanto@hotmail.com>
2026-03-29 19:08:21 -0300

    Game scene correction

--- go-to-town-game/scripts/game_over.gd
+++ go-to-town-game/scripts/game_over.gd
@@ -17,4 +17,7 @@ func _on_button_pressed():
 
 
 func _on_button_2_pressed():
+	get_tree().paused = false
+	GameState.intro_played = true
+	GameState.returning_from_options = false
 	get_tree().change_scene_to_file("res://scenes/ui/mainMenu.tscn")
