64716948f9d0c3416b8c0d35e075083c49817573
yanignacio <yandasilva3@gmail.com>
2026-03-29 02:00:03 -0300

    powerup changes

--- go-to-town-game/scripts/rocket.gd
+++ go-to-town-game/scripts/rocket.gd
@@ -1,8 +1,9 @@
 extends RigidBody2D
 
-const THRUST = 500
+const THRUST = 5000
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	apply_central_force(Vector2.RIGHT.rotated(rotation) * THRUST)
-	pass
+	if visible:
+		print(Vector2.RIGHT.rotated(rotation))
+		apply_central_force(Vector2.RIGHT.rotated(rotation) * THRUST)
